e1d7e3b9e920be745431136e6d49a4865559589b
BiraganiTejeswini <84894642+BiraganiTejeswini@users.noreply.github.com>
2021-06-07 10:10:55 +0530

    Update HelloController.java

--- src/main/java/com/hcl/controller/HelloController.java
+++ src/main/java/com/hcl/controller/HelloController.java
@@ -1,14 +1,1 @@
-package com.hcl.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class HelloController {
-
-	
-	@RequestMapping("/")
-	public String hello() {
-		return "Hello World";
-	}
-}
